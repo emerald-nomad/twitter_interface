24be12a406d2aa6589d30fe2bb860a7ac51f70e7
Terrell <terrell.greene95@gmail.com>
2018-06-03 00:20:31 -0500

    Added cookie parser and routes to application

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
+
+const routes = require('./routes');
+app.use(routes);
 
 app.listen(3000, () => console.log('The application is running on localhost:3000.'));
